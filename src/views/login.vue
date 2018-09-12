9f0db46d15c84fff0d6952961e39a9fa7ec939bd
lyw <2354640364@qq.com>
2018-09-13 02:32:01 +0800

    add login

---
+++ src/views/login.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <div class="wrap_box">
+        <div class="wrap_top">
+            <img src="../assets/image/login_paint.png" alt="">
+        </div>
+        <form @submit.prevent="onSubmit">
+        <div class="wrap_center">
+            <div class="input1">
+                <div id="u_input">
+                    <input type="text" :value="username">
+                </div>
+            </div>
+            <div class="input2">
+                <div id="p_input">
+                    <input type="text" :value="password">
+                </div>
+            </div>
+        </div>
+        <div class="wrap_bottom">
+            <button type="submit">登录</button>
+        </div>
+        </form>
+    </div>
+    <tip :title="tipTitle" v-show="tipTitle != ''"></tip>
+  </div>
+</template>
+
+<script>
+import Tip from '../components/Tip'
+export default {
+  data () {
+    return {
+      username: '请输入学号',
+      password: '请输入密码',
+      tipTitle: ''
+    }
+  },
+  components: {
+    Tip
+  },
+  methods: {
+    onSubmit () {
+      this.$axios.get('api/login', { 'userId': this.username, 'password': this.password })
+        .then((res) => {
+          if (res.status === 1) {
+            this.$router.push('/index')
+          } else {
+            console.log(res.status + ',' + res.message)
+            this.tipTitle = '学号或密码错误'
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrap_box{
+      margin: 0px;
+      padding: 0px;
+      width: 100vw;
+      height: 100vh;
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      background-image: url('../assets/image/login_background.png');
+  }
+  .wrap_top{
+      display: flex;
+      justify-content: center; /*子元素水平居中*/
+  }
+  .wrap_top img{
+      width: 71vw;
+      height: 55vh;
+  }
+  input{
+      border: none;
+      outline: none;
+      background-color: transparent;
+  }
+  .input1{
+      display: flex;
+      justify-content: center;
+      flex-direction:column;
+      align-items:center;
+  }
+  #u_input{
+      display: flex;
+      justify-content:center;
+      top: 56vh;
+      width: 56vw;
+      height: 7.5vh;
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      background-image: url('../assets/image/login_user.png');
+      z-index: 999;
+  }
+  #u_input input{
+      position: absolute;
+      width: 50vw;
+      height: 7.5vh;
+      padding-left: 22vw;
+      padding-top: 1vh;
+      font-family: PingFang-SC-Light;
+      font-size: 3vh;
+      font-weight: normal;
+      font-stretch: normal;
+      line-height: 7.5vh;
+      letter-spacing: 0px;
+      color: #ffffff;
+  }
+  .input2{
+      display: flex;
+      justify-content: center;
+      flex-direction:column;
+      align-items:center;
+  }
+  #p_input{
+      margin-top: 6vh;
+      display: flex;
+      justify-content:center;
+      top: 66vh;
+      width: 56vw;
+      height: 5vh;
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      background-image: url('../assets/image/login_password.png');
+      z-index: 999;
+  }
+  #p_input input{
+      position: absolute;
+      width: 50vw;
+      height: 4.5vh;
+      padding-left: 22vw;
+      font-family: PingFang-SC-Light;
+      font-size: 3vh;
+      font-weight: normal;
+      font-stretch: normal;
+      line-height: 4.5vh;
+      letter-spacing: 0px;
+      color: #ffffff;
+  }
+.wrap_bottom button{
+    position: absolute;
+    top: 85vh;
+    left: 26.5vw;
+    border: none;
+    width: 47vw;
+    height: 6.4vh;
+    background-color: #d7a2e6;
+    border-radius: 1vh;
+    font-family: PingFang-SC-Regular;
+    font-size: 2.5vh;
+    font-weight: normal;
+    font-stretch: normal;
+    line-height: 2.5vh;
+    letter-spacing: 0px;
+    color: #ffffff;
+  }
+</style>
